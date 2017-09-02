8e4b429b962a96b53856c2b6ec81cd681864c2d5
ThaoHuynh <flashvnn@gmail.com>
2017-09-02 16:59:43 +0700

    Add authentication

--- controllers/default.js
+++ controllers/default.js
@@ -1,12 +1,12 @@
-exports.install = function() {
-	F.route('/', view_index);
+exports.install = function () {
+    F.route('/', view_index);
     F.route('/login/github/', oauth_login, ['unauthorize']);
     F.route('/login/github/callback/', oauth_login_callback, ['unauthorize']);
 };
 
 function view_index() {
-	var self = this;
-	self.view('index');
+    var self = this;
+    self.view('index');
 }
 
 // Controller action
@@ -21,24 +21,37 @@ function oauth_login() {
     // oauth2.github.secret =
     // ...
 
-    MODULE('oauth2').redirect(type, CONFIG('oauth2.' + type + '.key'), self.host('/login/' + type + '/callback/'), self);
+    MODULE('oauth2').redirect(type, CONFIG('oauth2.' + type + '.key'), "https://totalsocial-flashvnn-1.c9users.io/login/github/callback", self);
 }
 
 // Controller action
 function oauth_login_callback() {
     var self = this;
     var type = self.req.path[1];
-    var url = self.host('/login/' + type + '/callback/');
-
-    // config:
-    // oauth2.google.key =
-    // oauth2.google.secret =
-    // oauth2.github.key =
-    // oauth2.github.secret =
-    // ...
-
-    MODULE('oauth2').callback(type, CONFIG('oauth2.' + type + '.key'), CONFIG('oauth2.' + type + '.secret'), url, self, function(err, profile, access_token) {
+    var url = "https://totalsocial-flashvnn-1.c9users.io/login/github/callback";
+    self.host('/login/' + type + '/callback/');
+    MODULE('oauth2').callback(type, CONFIG('oauth2.' + type + '.key'), CONFIG('oauth2.' + type + '.secret'), url, self, function (err, profile, access_token) {
         console.log(profile);
-        self.json(SUCCESS(true));
+
+        var email = profile.email;
+        var name = profile.name;
+        var nosql = NOSQL('users');
+        nosql.find().make(function (builder) {
+            builder.first();
+            builder.where('email', email);
+            builder.callback(function (err, response) {
+                var id;
+                if (!response) {
+                    var profiles = [];
+                    profiles['github'] = profile;
+                    id = UID();
+                    nosql.insert({id: id, email: email, name: name}).callback(NOOP);
+                } else {
+                    id = response.id;
+                }
+                controller.cookie(F.config.cookie, F.encrypt({ id: id, ip: controller.ip }, 'user'), '5 minutes');
+                callback(SUCCESS(true));
+            });
+        });
     });
 }
